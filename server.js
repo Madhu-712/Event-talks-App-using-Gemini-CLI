65525b306e5749775df6600db02d513d12521750
Madhu-712 <88141363+Madhu-712@users.noreply.github.com>
2025-09-14 14:48:03 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));
+
+app.get('/api/talks', (req, res) => {
+  fs.readFile(path.join(__dirname, 'talks.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading talks data');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
